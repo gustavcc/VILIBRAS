a762153acf71d4abd0b4db685193ee3d1fdad039
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2023-11-07 21:45:57 -0300

    dash

--- projeto_VILIBRAS/script/dashboard.js
+++ projeto_VILIBRAS/script/dashboard.js
@@ -1,42 +1,52 @@
 openMenu.addEventListener('click', () => {
     menu.style.display = 'flex',
-    slider.style.display = 'none'
+    containerSlide.style.display = 'none'
 
 })
 // função quando algo é clicado
 closeMenu.addEventListener('click', () => {
     menu.removeAttribute('style'),
-    slider.removeAttribute('style')
+    containerSlide.removeAttribute('style')
 })
 
-const imgs = document.querySelectorAll(".information");
-const dots = document.querySelectorAll(".botao i");
-const leftArrow = document.querySelector(".arrow-left");
-const rightArrow = document.querySelector(".arrow-right");
-let currentIndex = 0;
-let time = 5000; //Tempo padrão para apresentação de slides automática
-const defClass = (startPos, index) => {
-    for (let i = startPos; i < imgs.length; i++) {
-        imgs[i].style.display = "none";
-        dots[i].classList.remove("fa-dot-circle");
-        dots[i].classList.add("fa-circle");
+
+var proximaImagem = 1;//guarda a posição da imagem que deve entrar na tela, no caso é a imagem da posição 1, pois a imagem 0 já está visível na tela
+var zIndexAtual = 0;//usado para criar um aumento progressivo no z-index
+var imagens = document.getElementById("containerSlide").children;//pega todos os nodos filhos da div #container, ou seja, nossa coleção de imagens
+
+function avancarImagem(){
+    imagem = imagens[proximaImagem];//armazena a proxima imagem na fila em uma variável
+    imagem.style.zIndex = ++zIndexAtual;//aumenta o índice do z-index e atribui à próxima imagem da fila
+    imagem.style.marginLeft = "0%";//reseta a margem da imagem, fazendo ela entrar na tela
+
+    proximaImagem++;//ajusta o índice que aponta a próxima imagem
+
+    //caso o índice tenha alcançado o fim da fila, resetá-lo
+    if(proximaImagem >= imagens.length){
+        proximaImagem = 0;
     }
-    imgs[index].style.display = "block";
-    dots[index].classList.add("fa-dot-circle");
-};
-defClass(1, 0);
-leftArrow.addEventListener("click", function () {
-    currentIndex <= 0 ? currentIndex = imgs.length - 1 : currentIndex--;
-    defClass(0, currentIndex);
-});
-rightArrow.addEventListener("click", function () {
-    currentIndex >= imgs.length - 1 ? currentIndex = 0 : currentIndex++;
-    defClass(0, currentIndex);
-});
-const startAutoSlide = () => {
-    setInterval(() => {
-        currentIndex >= imgs.length - 1 ? currentIndex = 0 : currentIndex++;
-        defClass(0, currentIndex);
-    }, time);
-};
-startAutoSlide(); //Inicia o slideshow
+
+    //aqui está o truque para fazer com que as imagens que já passaram voltem para fora da área visível
+    //o tempo de intervalo está aqui para que isso ocorra depois que a imagem ficou atrás da nova imagem da fila
+    setTimeout(resetarImagens,500);
+}
+
+function resetarImagens(){
+    imagemVisivel = proximaImagem -1;
+
+    //se o índice alcançou o início da fila, voltar para o final
+    if(imagemVisivel < 0){
+        //o menos 1 está aqui pois o parâmetro length retorna o total de itens no array (no caso, 3)
+        //precisamos da posição do último item do array (sempre é length-1)
+        imagemVisivel = imagens.length - 1;
+    }
+
+    //retorna todas as imagens à sua posição original, menos a imagem visível
+    for(var i = 0; i < imagens.length; i++){
+        if(i != imagemVisivel){
+            imagens[i].style.marginLeft = "100%";
+        }
+    }
+}
+
+var intervalo = setInterval(avancarImagem,5000);
