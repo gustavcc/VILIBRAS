f5badd4079657225c03ae19102979254ba78bf1c
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2023-11-15 10:25:03 -0300

    dash

--- projeto_VILIBRAS/script/dashboard.js
+++ projeto_VILIBRAS/script/dashboard.js
@@ -2,10 +2,12 @@ openMenu.addEventListener('click', () => {
     menu.style.display = 'flex',
     containerSlide.style.display = 'none'
 
-})
+});
 // função quando algo é clicado
 closeMenu.addEventListener('click', () => {
     menu.removeAttribute('style'),
     containerSlide.removeAttribute('style')
-})
+});
+
+//slides de imagens
 
